235dab4771948228e5a4a792023ba273423a5b8f
EL MRAHI <hicham.elmrahi@gmail.com>
2026-04-16 11:53:30 +0000

    fix: supabase auth PKCE + persistSession

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
-
 const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL as string
 const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY as string
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    flowType: 'pkce',
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  }
+})
