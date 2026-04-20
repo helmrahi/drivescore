222804264d813b7f6acccb4c77b63ff6b50372b5
EL MRAHI <hicham.elmrahi@gmail.com>
2026-04-20 10:37:21 +0000

    fix: vite.config.ts vitest/config au lieu de vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/tests/setup.ts'],
     exclude: ['**/node_modules/**', '**/e2e/**'],
-    outputFile: './public/test-results.json',
   },
-  plugins: [react()],
 })
